bf2b4f490d7ccdf9970a1ad28e3e39e471d4ca75
Maisie <margaret.odorchaidhe@gmail.com>
2023-07-19 22:10:29 +0100

    track editor data implementation

--- Assets/Libraries/TrackEditor/Code/TrackEditorData.cs
+++ Assets/Libraries/TrackEditor/Code/TrackEditorData.cs
@@ -1,9 +1,11 @@
 #if UNITY_EDITOR
-using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEditor;
+using System.Linq;
+using System;
 
 // should not be making more of these, uncomment to be able to in case of emergency
 // [CreateAssetMenu(menuName = "TrackEditorData")]
@@ -11,12 +13,15 @@ using UnityEditor;
 public class TrackEditorData : ScriptableObject
 {
     public List<GameObject> trackSegmentPrefabs = new List<GameObject>();
+    
+    [HideInInspector]
+    public List<Track> tracks = new List<Track>();
+    private int currentTrack = -1;
+
     public string localPath;
     public string relativeDefaultTrackPath;
-    public List<string> customTrackSegmentsPaths = new List<string>();
-
     [HideInInspector]
-    public List<GameObject> tracks;
+    public List<string> customTrackSegmentsPaths = new List<string>();
 
     public void PopulateSegments(bool useDefaultAssets)
     {
@@ -47,9 +52,88 @@ public class TrackEditorData : ScriptableObject
         }
     }
 
-    public void CreateNewTrack()
+    public int CreateNewTrack()
     {
+        Assert.IsTrue(currentTrack == -1);
+        Track newTrack = new Track();
+        tracks.Add(newTrack);
+        currentTrack = tracks.Count - 1;
+        return currentTrack;
+    }
 
+    public void EditTrack(int index)
+    {
+        Assert.IsTrue(currentTrack == -1);
+        currentTrack = index;
+        tracks[index].unsavedChanges = true;
+    }
+
+    public void DiscardChangesToCurrentTrack()
+    {
+        if(tracks[currentTrack].isNew)
+        {
+            tracks.RemoveAt(currentTrack);
+        }
+        else
+        {
+
+            tracks[currentTrack].unsavedChanges = false;
+        }
+    }
+
+    public bool SaveCurrentTrack(string outputDirectory)
+    {
+        if(!Directory.Exists(outputDirectory))
+        {
+            Debug.Log("Output directory " + outputDirectory + " does not exist! Please create it or update the Output Directory Path and try again.");
+            return false;
+        }
+         
+        bool success = false;
+        if(tracks[currentTrack].isNew)
+        {
+            Assert.IsTrue(tracks[currentTrack].prefabPath == null);
+            if(tracks[currentTrack].parentObject != null)
+            {
+                Debug.Log("No track created! Can't save to disk.");
+                return false;
+            }
+            string fileName = new string(tracks[currentTrack].name.Where(c => !Char.IsWhiteSpace(c)).ToArray()); // remove all whitespace from the name given
+            tracks[currentTrack].prefabPath = outputDirectory + "/" + fileName + ".asset";
+            tracks[currentTrack].prefabPath = AssetDatabase.GenerateUniqueAssetPath(tracks[currentTrack].prefabPath);
+            PrefabUtility.SaveAsPrefabAssetAndConnect(tracks[currentTrack].parentObject, tracks[currentTrack].prefabPath, InteractionMode.UserAction, out success);
+            if(success)
+            {
+                tracks[currentTrack].isNew = false;
+                tracks[currentTrack].unsavedChanges = false;
+            }
+            else
+            {
+                tracks[currentTrack].prefabPath = null;
+            }
+        }
+        else
+        {
+            Assert.IsTrue(tracks[currentTrack].prefabPath != null && tracks[currentTrack].parentObject != null);
+            PrefabUtility.SavePrefabAsset(tracks[currentTrack].parentObject, out success);
+            if(success)
+            {
+                tracks[currentTrack].unsavedChanges = false;
+            }
+        }
+        
+        return success;
     }
 }
+
+public class Track
+{
+    public string name = "Unnamed Track";
+    public GameObject parentObject = null;
+    public List<GameObject> trackSegments = new List<GameObject>();
+    public bool isNew = true;
+    public bool unsavedChanges = false;
+    public string prefabPath = null;
+}
+
 #endif
