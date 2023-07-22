0ee6b3ab7a85cef51a6691b55edc4c5da5c6a97f
Maisie <margaret.odorchaidhe@gmail.com>
2023-07-22 22:16:06 +0100

    tester for fizz buzz + maisie's solution

---
+++ Assets/Beans/Test/Coding Challenges/Maisie/FizzBuzz.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FizzBuzzSolution_Maisie : FizzBuzzSolution
+{
+    public override IList<string> FizzBuzz(int n) {
+        List<string> answer = new List<string>();
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 15 == 0) // can do this because both 3 and 5 have no shared factors, this would not work otherwise
+            {
+                answer.Add("FizzBuzz");
+            }
+            else if(i % 5 == 0)
+            {
+                answer.Add("Buzz");
+            }
+            else if(i % 3 == 0)
+            {
+                answer.Add("Fizz");
+            }
+            else
+            {
+                answer.Add(i.ToString());
+            }
+        }
+
+        return answer;
+    }
+}
